ef63158ed1c5f9b3d5e233da68557b2df6ab534c
MangoAbricosik <70761769+MangoAbricosik@users.noreply.github.com>
2020-10-12 04:58:41 +0300

    Raslabon

---
+++ Solo 2/Solo 2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Solo_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Сколько нужно пиромид - ");
+            int sus = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Высота пирамид - ");
+            int kol = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Отступ пирамид - ");
+            int ots = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            int count = 0;
+
+           
+            for (int z = 0; z < sus; z++)
+            {          
+                    for (int i = 0; i < kol; i++)
+                    {
+                        Console.WriteLine();
+                        count++;
+
+                        for (int k = 0; k < count; k++)
+                        {
+                            Console.Write("#");
+                      
+
+                    }
+
+                    }
+                    for (int j = 0; j < ots; j++)
+                    {
+                        Console.WriteLine();
+                    }
+
+                    for (int i = kol; i > 0; i--)
+                    {
+                        Console.WriteLine();
+                        count--;
+                        for (int k = -1; k < count; k++)
+                        {
+                            Console.Write("#");
+                        
+
+                    }
+                    }
+                
+                }
+        }
+    }
+}
